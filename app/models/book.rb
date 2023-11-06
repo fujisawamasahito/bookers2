0bd5d893c64f8a79c4ba28856577c047671c73b8
EC2 Default User <fujisawa.masahito@gmail.com>
2023-11-06 10:21:45 +0000

    Commit4

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,7 +10,7 @@ class Book < ApplicationRecord
     length: { maximum: 200 }
 
     def favorited_by?(user)
-    favorites.exists?(user_id: user.id)
-  end
+      favorites.exists?(user_id: user.id)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    resources :books, only: [:new, :index, :show, :create, :destroy, :edit, :update] do
-    resource :favorite, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
    end
     resources :users, only: [:show, :edit, :update, :index]
